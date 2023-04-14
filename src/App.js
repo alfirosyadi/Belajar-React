823aa2bb4822c77c242e8b8efe8854cce5ef5073
alfirosyadi <muhammadalfirosyadi1998@gmail.com>
2023-04-14 20:40:06 +0700

    belajar useeffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Introduction from './components/Introduction';
@@ -8,7 +8,20 @@ import Button from './components/Button';
 
 function App() {
 
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(() => {
+    console.log("useState run")
+    return 0
+  })
+
+  useEffect(
+    () => {
+      if (count === 4){
+        console.log('useEffect run')
+        return alert('angka sama dengan 4');
+      }
+    },
+    [count]
+  )
 
   function decrementCount() {
     setCount(count => count - 1)
